ac467ab72be40e80099ef277ca75c367f5bff182
Guillaume Aubert <guillaume@aubert.ws>
2025-02-07 10:56:41 +0100

    feat: Add private attributes, getter setters and total() function to CartItem class

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -7,40 +7,54 @@ const InvalidPriceException = require("./InvalidPriceException.js");
 module.exports = class CartItem {
 
     //region private attributes
+    #articleId;
+    #name;
+    #quantity;
+    #price;
     //endregion private attributes
 
     //region public methods
     constructor(articleId, name, quantity, price) {
-	throw new Error();
+        this.#articleId = articleId;
+        this.#name = name;
+        this.#quantity = quantity;
+        this.#price = price
     }
 
     get articleId() {
-        throw new Error();
+        return this.#articleId;
     }
 
     get name() {
-        throw new Error();
+        return this.#name;
     }
 
     get quantity() {
-        throw new Error();
+        return this.#quantity;
     }
 
     set quantity(value) {
-        throw new Error();
+        if (value < 0) {
+            throw new Error("Quantity cannot be less than zero.");
+        }
+        this.#quantity = value;
     }
 
     get price() {
-        throw new Error();
+        return this.#price;
     }
 
     set price(value) {
-        throw new Error();
+        if (value < 0) {
+            throw new Error("Price cannot be less than zero.");
+        }
+        this.#price = value;
     }
 
     get total() {
-        throw new Error();
+        return this.#quantity * this.#price;
     }
+
     //endregion public methods
 
     //region private methods
